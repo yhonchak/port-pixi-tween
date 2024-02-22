d2206d0e87a60d4f524b728060b30bb160721f5b
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-22 17:26:17 +0200

    feat(port): add 4 `docks` to the port

--- src/port.class.ts
+++ src/port.class.ts
@@ -1,22 +1,36 @@
 import * as PIXI from 'pixi.js';
+import { Dock } from './dock.class';
 
 export class Port {
     private readonly bg: number = 0x17577E;
 
-    private app: PIXI.Application;
+    private static readonly dockWidth: number = 40;
+    private static readonly dockHeight: number = 120;
+
+    private readonly app: PIXI.Application;
     private width: number;
     private height: number;
+    private docks: Dock[];
 
     constructor(app: PIXI.Application, width: number, height: number) {
         this.app = app;
         this.width = width;
         this.height = height;
+        this.docks = [];
 
         // Create port graphics
-        const portGraphics = new PIXI.Graphics();
+        const portGraphics: PIXI.Graphics = new PIXI.Graphics();
         portGraphics.beginFill(this.bg);
         portGraphics.drawRect(0, 0, width, height);
         portGraphics.endFill();
         this.app.stage.addChild(portGraphics);
+
+        // Create docks
+        for (let i: number = 0; i < 4; i++) {
+            const dockX: number = 0;
+            const dockY: number = i * (Port.dockHeight + 20) + 30; // Adjust for spacing between docks
+            const dock: Dock = new Dock(this.app, dockX, dockY, Port.dockWidth, Port.dockHeight);
+            this.docks.push(dock);
+        }
     }
 }
