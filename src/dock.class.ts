4b9733084a0305ff5c6cab19973ee0f6e499111b
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-23 16:48:56 +0200

    fix(dock): set object position to its sprite

--- src/dock.class.ts
+++ src/dock.class.ts
@@ -5,13 +5,24 @@ export class Dock {
     private app: PIXI.Application;
     private dockGraphics: PIXI.Graphics;
 
+    /**
+     * The class constructor.
+     *
+     * @param app - link to the Pixi Application
+     * @param x - the dock `x` position
+     * @param y - the dock `y` position
+     * @param width - the dock width
+     * @param height - the dock height
+     */
     constructor(app: PIXI.Application, x: number, y: number, width: number, height: number) {
         this.app = app;
 
         this.dockGraphics = new PIXI.Graphics();
         this.dockGraphics.beginFill(0xffff00);
-        this.dockGraphics.drawRect(x, y, width, height);
+        this.dockGraphics.drawRect(0, 0, width, height);
         this.dockGraphics.endFill();
+        this.dockGraphics.x = x;
+        this.dockGraphics.y = y;
         this.app.stage.addChild(this.dockGraphics);
     }
 
