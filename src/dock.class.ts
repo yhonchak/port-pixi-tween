59204c661a3bc3dbaf82fb2776b9197e24239779
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-23 15:24:00 +0200

    feat(dock): add getter to return the object position

--- src/dock.class.ts
+++ src/dock.class.ts
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import { Position } from './types';
 
 export class Dock {
     private app: PIXI.Application;
@@ -12,5 +13,16 @@ export class Dock {
         this.dockGraphics.drawRect(x, y, width, height);
         this.dockGraphics.endFill();
         this.app.stage.addChild(this.dockGraphics);
+    }
+
+    /**
+     * Returns the dock position.
+     * @returns Position.
+     */
+    get position(): Position {
+        return {
+            x: this.dockGraphics.x,
+            y: this.dockGraphics.y
+        }
     }
 }
