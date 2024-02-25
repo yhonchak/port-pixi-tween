d824b50b95abdcf1842ed0ee3651a9d5ade18f97
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-25 15:42:07 +0200

    feat(utils): add method to provide async delay

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
 /**
  * Keeps utility functions and other simple tools.
  */
+
+/**
+ * Returns a promise with a delay.
+ *
+ * @param ms is a time interval in milliseconds
+ */
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
