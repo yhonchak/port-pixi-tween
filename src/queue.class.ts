721b95f0e4be2fb5a9e10b73bf29605aef6acdea
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-27 14:42:06 +0200

    feat(queue): create initial class

---
+++ src/queue.class.ts
@@ -0,0 +1,29 @@
+import * as PIXI from 'pixi.js';
+
+export class Queue {
+
+    private app: PIXI.Application;
+    private readonly sprite: PIXI.Graphics;
+
+    /**
+     * The class constructor.
+     *
+     * @param app - reference to the Pixi Application
+     * @param x - the queue `x` position
+     * @param y - the queue `y` position
+     */
+    constructor(app: PIXI.Application, x: number = 0, y: number = 0) {
+        this.app = app;
+
+        this.sprite = new PIXI.Graphics();
+        // temporary graphics
+        this.sprite.beginFill(0x000000);
+        this.sprite.drawRect(0, 0, 25, 25);
+        this.sprite.endFill();
+
+        this.sprite.x = x;
+        this.sprite.y = y;
+
+        this.app.stage.addChild(this.sprite);
+    }
+}
